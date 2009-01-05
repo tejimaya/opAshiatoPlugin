a5b38d66977e2093593a50d70e506cb6498cee07
Kousuke Ebihara <ebihara@tejimaya.com>
2009-01-05 14:44:56 +0000

    #3276:corrected docblock comment
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opAshiatoPlugin/trunk@9930 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/ashiato/actions/actions.class.php
+++ apps/pc_frontend/modules/ashiato/actions/actions.class.php
@@ -5,8 +5,7 @@
  *
  * @package    OpenPNE
  * @subpackage ashiato
- * @author     Your name here
- * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
+ * @author     Satoru Yamane <yamane@icz.co.jp>
  */
 class ashiatoActions extends sfActions
 {
