bf15858f3d191d363ef03c6aec9845ae4e4d87ec
Kousuke Ebihara <ebihara@tejimaya.com>
2009-01-24 18:27:02 +0000

    #3360:added "back to previous page" to the every *Error templates of the pc_* application
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opAshiatoPlugin/trunk@10362 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/ashiato/templates/listError.php
+++ apps/pc_frontend/modules/ashiato/templates/listError.php
@@ -1,1 +1,4 @@
 <?php include_box('noAshiato', __('Pageview logs'), __('no other Pageview logs')) ?>
+
+<?php use_helper('Javascript') ?>
+<p><?php echo link_to_function(__('前のページに戻る'), 'history.back()') ?></p>
