5f809af72e11811e0c5ffac3cd53eb4001c3899c
Rimpei Ogawa <rimpei.ogawa@gmail.com>
2009-01-27 03:43:54 +0000

    opAshiatoPlugin: op_include_line
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/plugins/opAshiatoPlugin/trunk@10477 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/ashiato/templates/listError.php
+++ apps/pc_frontend/modules/ashiato/templates/listError.php
@@ -1,4 +1,4 @@
 <?php include_box('noAshiato', __('Pageview logs'), __('no other Pageview logs')) ?>
 
 <?php use_helper('Javascript') ?>
-<p><?php echo link_to_function(__('前のページに戻る'), 'history.back()') ?></p>
+<?php op_include_line('backLink', link_to_function(__('Back to previous page'), 'history.back()')) ?>
